d424383d8062326a850d2c0d75a19dff3da6bcec
ifeanyiifechidelu <102835210+ifeanyiifechidelu@users.noreply.github.com>
2022-05-28 14:36:03 +0100

    Adding file

---
+++ NonNegativeInt/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+    static int DescendingOrder(int num)
+    {
+        // Bust a move right here
+        var value = "";
+        var numbers = num.ToString().ToCharArray();
+
+        Array.Sort(numbers);
+        Array.Reverse(numbers);
+
+        value = string.Join("", numbers);
+
+
+        return Convert.ToInt32(value);
+    }
